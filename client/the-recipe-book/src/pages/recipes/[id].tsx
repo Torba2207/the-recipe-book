eb06aec92c527fd4ffab3885834df89a61580b0a
Torba2207 <alexnychyporchuk@gmail.com>
2025-04-25 19:36:01 +0200

    feat: Meals of category fetched and Side Bar added

--- client/the-recipe-book/src/pages/recipes/[id].tsx
+++ client/the-recipe-book/src/pages/recipes/[id].tsx
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
+
+import MealsOfCategory from "@/components/MealsOfCategory"; 
 import Head from "next/head";
 
 
@@ -19,7 +21,9 @@ export default function RecipePage() {
   const { id } = router.query;
 
   const [recipe, setRecipe] = useState<Recipe|null>(null);
-  const [loading, setLoading] = useState(true);
+  const [meals, setMeals] = useState([]);
+  const [loading, setLoading] = useState(true)
+  const [secondLoading, setSecondLoading] = useState(true);
 
   useEffect(() => {
     if (!id) return;
@@ -37,11 +41,34 @@ export default function RecipePage() {
         setLoading(false);
       }
     };
+    
+    
 
     fetchRecipe();
+    
   }, [id]);
 
-  if (loading) return <p>Loading...</p>;
+  useEffect(() => {
+    const fetchMeals=async () => {
+        try {
+            const response = await fetch(`http://localhost:3000/api/recipes/available?category=${recipe?.strCategory}`,{
+                method: "GET",
+            });
+            const data = await response.json();
+            setMeals(data.meals);
+            console.log(data.meals)
+        }
+        catch (error) {
+            console.error("Error fetching recipe:", error);
+        }
+        finally {
+            setSecondLoading(false);
+        }
+    }
+    fetchMeals()
+    },[recipe])
+
+  if (loading||secondLoading) return <p>Loading...</p>;
   if (!recipe) return <p>Recipe not found</p>;
 
   return (
@@ -49,12 +76,18 @@ export default function RecipePage() {
       <Head>
         <title>{recipe.strMeal}</title>
       </Head>
-      <div className="p-4">
+      <div className="flex flex-col lg:flex-row w-full px-4 gap-4">
+      <div className="p-4 w-[60%]">
         <h1 className="text-3xl font-bold">{recipe.strMeal}</h1>
         <p className="italic">{recipe.strArea} - {recipe.strCategory}</p>
         <img src={recipe.strMealThumb} alt={recipe.strMeal} className="my-4 w-full max-w-md rounded-xl shadow" />
         <p className="mt-4 whitespace-pre-wrap">{recipe.strInstructions}</p>
         {/* Optional: ingredients table */}
+      </div>
+      <aside  className="lg:w-1/4 p-4 border-l border-gray-200">
+        {meals!==null && <MealsOfCategory className={"space-y-4"} recipes={meals} />}
+        
+      </aside>
       </div>
     </>
   );
