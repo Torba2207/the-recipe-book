a22c4128bc9511a9a1c342cf345577690a173135
Torba2207 <alexnychyporchuk@gmail.com>
2025-04-25 19:36:41 +0200

    feat: Component of SideBar for single recipe page

---
+++ client/the-recipe-book/src/components/MealsOfCategory.tsx
@@ -0,0 +1,33 @@
+interface Recipe {
+    idMeal: string;
+    strMeal: string;
+    strCategory: string;
+    strArea: string;
+    strMealThumb: string;
+  }
+
+  interface RecipesListProps {
+    recipes: Recipe[];
+    className?: string;
+  }
+
+export default function MealsOfCategory({ recipes, ...props }: RecipesListProps) {
+    return (
+        <div className={props.className}>
+            <div className="column">
+                {recipes.slice(0,4).map((meal) => (
+                    <div key={meal.idMeal} className="col-md-4">
+                        <div className="card mb-4">
+                            <img src={meal.strMealThumb} className="card-img-top" alt={meal.strMeal} />
+                            <div className="card-body">
+                                <h5 className="card-title">{meal.strMeal}</h5>
+                                <p className="card-text">{meal.strCategory}</p>
+                                {/*<a href={`/recipes/${meal.idMeal}`} className="btn btn-primary">View Recipe</a>*/}
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
